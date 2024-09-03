88ca9a5a9816a47f307840229e4da12678b78924
schmidlothar1 <schmidlothar1@gmail.com>
2024-09-03 12:07:59 +0000

    Add Rectangle

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 First 
 https://www.youtube.com/watch?v=PsTUuID27KA&t=3982s
+Next: 1:40:28
 
--- main.js
+++ main.js
@@ -14,17 +14,59 @@ const stageProperties = {
     top: canvasProperties.center.y - 480 / 2,
     background: 'white',
 };
-const ctx = myCanvas.getContext("2d");
 
 myCanvas.width = canvasProperties.width;
 myCanvas.height = canvasProperties.height;
+
+const ctx = myCanvas.getContext("2d");
+clearCanvas();
 
 
 
 const shapes = [];
 let path = [];
-clearCanvas();
+let rectangle = {};
 
+
+myCanvas.addEventListener('pointerdown', (e) => {
+    const mousePosition = {
+        x: e.offsetX,
+        y: e.offsetY,
+    };    
+    rectangle.corner1 = mousePosition;
+
+    const moveCallback = function(e) {
+        const mousePosition = {
+            x: e.offsetX,
+            y: e.offsetY,
+        };
+        rectangle.corner2 = mousePosition;
+
+        clearCanvas();
+        for(const shape of [...shapes, rectangle]) {
+            ctx.beginPath();
+            ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+            const rect = shape;
+            const minX = Math.min(rect.corner1.x, rect.corner2.x);
+            const minY = Math.min(rect.corner1.y, rect.corner2.y);
+            const width = Math.abs(rect.corner1.x - rect.corner2.x);
+            const height = Math.abs(rect.corner1.y - rect.corner2.y);
+            ctx.rect(minX, minY, width, height);
+            ctx.stroke();  
+        }          
+    }
+    const upCallback = function(e) {
+        myCanvas.removeEventListener("pointermove", moveCallback);
+        myCanvas.removeEventListener("pointerup", upCallback);
+
+        shapes.push(rectangle);
+        rectangle = {};       
+       
+    };
+    myCanvas.addEventListener('pointermove', moveCallback);
+    myCanvas.addEventListener('pointerup', upCallback);
+});
+/*
 myCanvas.addEventListener('pointerdown', (e) => {
     const mousePosition = {
         x: e.offsetX,
@@ -60,7 +102,7 @@ myCanvas.addEventListener('pointerdown', (e) => {
     myCanvas.addEventListener('pointermove', moveCallback);
     myCanvas.addEventListener('pointerup', upCallback);
 });
-
+*/
 function clearCanvas() {
     // shapes.length = 0;
     ctx.fillStyle = canvasProperties.background;
